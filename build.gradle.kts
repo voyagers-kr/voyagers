d64e5387994fa6928ad21530ac1b92ef81e8d843
Zin Park <zin@splab.dev>
2023-09-24 10:55:43 +0900

    단일 모듈 build.gradle 설정으로 rollback

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,45 +4,34 @@ plugins {
 	id("org.springframework.boot") version "3.1.3"
 	id("io.spring.dependency-management") version "1.1.3"
 	kotlin("jvm") version "1.8.22"
-	kotlin("plugin.spring") version "1.8.22" apply false
+	kotlin("plugin.spring") version "1.8.22"
 }
 
-java.sourceCompatibility = JavaVersion.VERSION_17
+group = "at"
+version = "0.0.1-SNAPSHOT"
 
-allprojects {
-	group = "at.voyagers"
-	version = "0.0.1-SNAPSHOT"
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
 
-	repositories {
-		mavenCentral()
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "17"
 	}
 }
 
-subprojects {
-	apply(plugin = "org.springframework.boot")
-	apply(plugin = "io.spring.dependency-management")
-	apply(plugin = "org.jetbrains.kotlin.plugin.spring")
-	apply(plugin = "kotlin")
-
-	dependencies {
-		implementation("org.springframework.boot:spring-boot-starter")
-		implementation("org.springframework.boot:spring-boot-starter-web")
-
-		implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-		implementation("org.jetbrains.kotlin:kotlin-reflect")
-		implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
-		testImplementation("org.springframework.boot:spring-boot-starter-test")
-	}
-
-	tasks.withType<KotlinCompile> {
-		kotlinOptions {
-			freeCompilerArgs += "-Xjsr305=strict"
-			jvmTarget = "17"
-		}
-	}
-
-	tasks.withType<Test> {
-		useJUnitPlatform()
-	}
+tasks.withType<Test> {
+	useJUnitPlatform()
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,1 @@
 rootProject.name = "voyagers"
-include("domain")
