db35763589d1012b880d690fc7674984e127b0a7
kiljaewon <kiljw316@gmail.com>
2023-10-08 13:54:49 +0900

    feat: 클래스 멤버 위치 변경

--- src/main/kotlin/at/voyagers/common/account/domain/Account.kt
+++ src/main/kotlin/at/voyagers/common/account/domain/Account.kt
@@ -1,8 +1,8 @@
 package at.voyagers.common.account.domain
 
 data class Account(
-    var id: String?,
     val email: String,
     val password: String,
     val role: AccountRole,
+    var id: String? = null,
 )
