132471ca69d52a28d28506d546d652e2db022e3f
jaesungahn91 <jamie.ahn@nsuslab.com>
2023-10-13 21:53:53 +0900

    feat: ui 영역 작성

---
+++ src/main/kotlin/at/voyagers/common/order/ui/OrderController.kt
@@ -0,0 +1,42 @@
+package at.voyagers.common.order.ui
+
+import at.voyagers.common.order.application.OrderService
+import at.voyagers.common.order.domain.Order
+import at.voyagers.common.order.domain.OrderAddress
+import at.voyagers.common.order.domain.OrderCreateSpec
+import at.voyagers.common.order.domain.OrderVariant
+import org.springframework.http.HttpStatus.CREATED
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class OrderController(
+    private val orderService: OrderService
+) {
+
+    @ResponseStatus(CREATED)
+    @PostMapping("/orders")
+    fun postOrder(@RequestBody request: OrderCreateRequest): Order {
+        return orderService.create(request.toOrderCreateSpec())
+    }
+
+}
+
+data class OrderCreateRequest(
+    val customerId: String,
+    val vendorId: String,
+    val orderVariants: MutableList<OrderVariant>,
+    val customerAddress: OrderAddress,
+) {
+    fun toOrderCreateSpec(): OrderCreateSpec {
+        return OrderCreateSpec(
+            customerId = customerId,
+            vendorId = vendorId,
+            orderVariants = orderVariants,
+            customerAddress = customerAddress,
+        )
+    }
+}
+
