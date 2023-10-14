f001929d62081ef4e61a7a1780f89e14978ab43a
Zin Park <zin@splab.dev>
2023-10-14 14:52:53 +0900

    Product가 생성될 때 Variant도 생성될 수 있도록 도메인 method 추가

--- src/main/kotlin/at/voyagers/common/variant/domain/Variant.kt
+++ src/main/kotlin/at/voyagers/common/variant/domain/Variant.kt
@@ -4,4 +4,14 @@ data class Variant(
     val id: String,
     val productId: String,
     val quantity: Int,
-)
+) {
+    companion object {
+        fun create(productId: String, quantity: Int): Variant {
+            return Variant(
+                id = "1",
+                productId = productId,
+                quantity = quantity,
+            )
+        }
+    }
+}
